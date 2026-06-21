317a5dccc39081e8367dfb85753ef628d0505e3d
avokado03 <graure@yandex.ru>
2026-06-21 15:09:47 +0500

    raw observation

--- Domain/RawObservations.cs
+++ Domain/RawObservations.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain
+﻿namespace Domain
 {
+    /// <summary>
+    /// Сырые наблюдения, загруженные из внешних источников, до их обработки и трансформации.
+    /// </summary>
     public class RawObservations
     {
+        public Guid Id { get; set; }
+        public Guid StreamId { get; set; }
+        public Guid RunId { get; set; }
+        public required string ExternalObservationId { get; set; }
+        public required string RawJson { get; set; }
+        public DateTime IngestedAt { get; set; }
+        public required string SourceName { get; set; }
+        public DateTime LoadDate { get; set; }
     }
 }
